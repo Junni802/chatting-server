6adb7b8872b8a130ff1b947a717c61b1fab61673
covy <junni802@naver.com>
2025-06-11 18:31:40 +0900

    feat[WebSocket]: add Chatting-server

---
+++ src/test/java/covy/chattingserver/ChattingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package covy.chattingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
